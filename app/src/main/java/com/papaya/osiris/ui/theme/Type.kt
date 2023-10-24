2306f73cfbd9584a0f1678ff9ef8586695b6f2d7
davitorress <dev.davitorres@gmail.com>
2023-10-24 11:40:31 -0300

    feat: add Typography for app

--- app/src/main/java/com/papaya/osiris/ui/theme/Type.kt
+++ app/src/main/java/com/papaya/osiris/ui/theme/Type.kt
@@ -3,32 +3,97 @@ package com.papaya.osiris.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.papaya.osiris.R
 
-// Set of Material typography styles to start with
+val GoogleFrontProvider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val NunitoFont = GoogleFont("Nunito")
+val UbuntuFont = GoogleFont("Ubuntu")
+
+val NunitoFamily = FontFamily(
+    Font(googleFont = NunitoFont, fontProvider = GoogleFrontProvider, style = FontStyle.Normal)
+)
+val UbuntuFamily = FontFamily(
+    Font(googleFont = UbuntuFont, fontProvider = GoogleFrontProvider, style = FontStyle.Normal)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        lineHeight = 42.sp,
+    ),
+    titleMedium = TextStyle(
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp,
+        lineHeight = 34.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 30.sp,
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = NunitoFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = NunitoFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+    ),
+
+    displayLarge = TextStyle(
+        fontFamily = UbuntuFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 30.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = UbuntuFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 30.sp,
+    ),
 )
