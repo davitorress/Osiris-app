69966b8e5059639be175aa758e87c3ce9f6e3827
davitorress <dev.davitorres@gmail.com>
2023-10-24 11:39:43 -0300

    feat: add google fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.papaya.osiris"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.papaya.osiris"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -56,6 +56,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
