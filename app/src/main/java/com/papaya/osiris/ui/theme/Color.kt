fd96ca1b67db3c826620e46cf8664c62d6ebadf7
davitorress <dev.davitorres@gmail.com>
2023-10-24 11:56:41 -0300

    style: change color values to kotlin pattern

--- app/src/main/java/com/papaya/osiris/ui/theme/Color.kt
+++ app/src/main/java/com/papaya/osiris/ui/theme/Color.kt
@@ -2,15 +2,15 @@ package com.papaya.osiris.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Black = Color(17, 17, 17, 255)
-val White = Color(255, 255, 255, 255)
+val Black = Color(0xFF111111)
+val White = Color(0xFFFFFFFF)
 
-val Gray = Color(242, 242, 242, 255)
-val GrayBorder = Color(209, 209, 209, 255)
-val DarkGray = Color(169, 169, 169, 255)
+val Gray = Color(0xFFF2F2F2)
+val GrayBorder = Color(0xFFD1D1D1)
+val DarkGray = Color(0xFFA9A9A9)
 
-val LightGreen = Color(0, 219, 51, 255)
-val MediumGreen = Color(0, 168, 39, 255)
-val DarkGreen = Color(0, 92, 21, 255)
+val LightGreen = Color(0xFF00DB33)
+val MediumGreen = Color(0xFF00A827)
+val DarkGreen = Color(0xFF005C15)
 
-val Wine = Color(128, 0, 0, 255)
+val Wine = Color(0xFF800000)
