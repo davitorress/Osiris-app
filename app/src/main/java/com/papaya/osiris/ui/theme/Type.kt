f58057c71b04d34911d740e16e1dd727406e8089
davitorress <dev.davitorres@gmail.com>
2023-10-25 15:27:09 -0300

    style: remove all lineHeight

--- app/src/main/java/com/papaya/osiris/ui/theme/Type.kt
+++ app/src/main/java/com/papaya/osiris/ui/theme/Type.kt
@@ -31,69 +31,58 @@ val Typography = Typography(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 36.sp,
-        lineHeight = 42.sp,
     ),
     titleMedium = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 28.sp,
-        lineHeight = 34.sp,
     ),
     titleSmall = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 24.sp,
-        lineHeight = 30.sp,
     ),
 
     bodyLarge = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
-        lineHeight = 24.sp,
     ),
     bodyMedium = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
-        lineHeight = 20.sp,
     ),
     bodySmall = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
-        lineHeight = 18.sp,
     ),
 
     labelLarge = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 20.sp,
-        lineHeight = 26.sp,
     ),
     labelMedium = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 18.sp,
-        lineHeight = 24.sp,
     ),
     labelSmall = TextStyle(
         fontFamily = NunitoFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
-        lineHeight = 22.sp,
     ),
 
     displayLarge = TextStyle(
         fontFamily = UbuntuFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
-        lineHeight = 30.sp,
     ),
     displayMedium = TextStyle(
         fontFamily = UbuntuFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
-        lineHeight = 30.sp,
     ),
 )
