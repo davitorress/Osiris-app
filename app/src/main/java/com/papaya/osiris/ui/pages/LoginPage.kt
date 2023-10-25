8d1de0aee63bda5f3b064f1da13125f2c74b26aa
davitorress <dev.davitorres@gmail.com>
2023-10-25 15:30:33 -0300

    feat: LoginPage design

---
+++ app/src/main/java/com/papaya/osiris/ui/pages/LoginPage.kt
@@ -0,0 +1,102 @@
+package com.papaya.osiris.ui.pages
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.papaya.osiris.R
+import com.papaya.osiris.ui.components.*
+import com.papaya.osiris.ui.theme.MediumGreen
+import com.papaya.osiris.ui.theme.OsirisTheme
+import com.papaya.osiris.ui.theme.White
+
+@Composable
+fun LoginPage() {
+    var email by rememberSaveable { mutableStateOf("") }
+    var senha by rememberSaveable { mutableStateOf("") }
+    var isFarmer by rememberSaveable { mutableStateOf(false) }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .background(White)
+            .padding(vertical = 0.dp, horizontal =  22.dp),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize().fillMaxWidth().padding(top = 96.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(36.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.osiris_logo),
+                contentDescription = "Osiris",
+                alignment = Alignment.Center
+            )
+
+            Column(verticalArrangement = Arrangement.spacedBy(26.dp)) {
+                EmailInput(
+                    text = email,
+                    onTextChange = { email = it }
+                )
+                PasswordInput(
+                    text = senha,
+                    onTextChange = { senha = it }
+                )
+                CheckboxInput(
+                    text = "Entrar como um agricultor",
+                    checked = isFarmer,
+                    onCheckedChange = { isFarmer = it }
+                )
+            }
+
+            ThemedButton(
+                onClick = {  },
+                theme = ButtonTheme.Medium,
+                text = "Entrar"
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start)
+            ) {
+                Text(
+                    text = "Já possui uma conta?",
+                    color = MediumGreen,
+                    style = MaterialTheme.typography.labelSmall
+                )
+                ThemedTextButton(
+                    onClick = {  },
+                    text = "Entre aqui",
+                    theme = ButtonTheme.Wine,
+                    modifier = Modifier,
+                    fontWeight = FontWeight.Black,
+                    textDecoration = TextDecoration.Underline
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 400)
+@Composable
+fun LoginPagePreview() {
+    OsirisTheme {
+        LoginPage()
+    }
+}
