fbb8422b36a9b283f4f49788e5be6d3a54abc7bb
davitorress <dev.davitorres@gmail.com>
2023-10-25 19:48:04 -0300

    feat: add RegisterPage design

--- app/src/main/java/com/papaya/osiris/MainActivity.kt
+++ app/src/main/java/com/papaya/osiris/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.papaya.osiris.ui.pages.LoginPage
+import com.papaya.osiris.ui.pages.RegisterPage
 import com.papaya.osiris.ui.theme.OsirisTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginPage()
+                    RegisterPage()
                 }
             }
         }
