7b31221fdf62734093027441b1d7ad3fd0c5d49f
davitorress <dev.davitorres@gmail.com>
2023-10-26 21:54:48 -0300

    test: HomePage test build

--- app/src/main/java/com/papaya/osiris/MainActivity.kt
+++ app/src/main/java/com/papaya/osiris/MainActivity.kt
@@ -3,8 +3,7 @@ package com.papaya.osiris
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.papaya.osiris.ui.components.CardsCarousel
-import com.papaya.osiris.ui.components.Product
+import com.papaya.osiris.ui.pages.HomePage
 import com.papaya.osiris.ui.pages.LoginPage
 import com.papaya.osiris.ui.pages.RegisterPage
 import com.papaya.osiris.ui.theme.OsirisTheme
@@ -14,14 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OsirisTheme {
-                CardsCarousel(
-                    listOf(
-                        Product("Item 1", "https://picsum.photos/400"),
-                        Product("Item 2", "https://picsum.photos/300"),
-                        Product("Item 3", "https://picsum.photos/700"),
-                        Product("Item 4", "https://picsum.photos/500"),
-                    )
-                )
+                HomePage()
             }
         }
     }
