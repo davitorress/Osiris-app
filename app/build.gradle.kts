df849c489ef741a73431e6f0284a863b5eadd59f
davitorress <dev.davitorres@gmail.com>
2023-11-08 20:21:14 -0300

    feat: add compose navigation dependencie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
     implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.compose.foundation:foundation:1.5.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
